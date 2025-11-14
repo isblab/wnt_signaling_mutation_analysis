efd3abab7e732a5fe0fa110dd1ff1671bf1f3d7d
omgol411 <omgolatkar2001@gmail.com>
2025-11-14 16:03:16 +0530

    include aromatic ring interactions parser

--- scripts/arpeggio_constants.py
+++ scripts/arpeggio_constants.py
@@ -1,4 +1,4 @@
-CONTACT_FIELDS = [
+CONTACTS_FIELDS = [
     "atom_1",
     "atom_2",
     "clash",
@@ -30,4 +30,28 @@ CHOSEN_CONTACT_TYPES = [
     "carbonyl",
     "polar",
     "weak_polar",
+]
+
+RI_FIELDS = [
+    "ring1_id",
+    "ring1_residue",
+    "ring1_centroid",
+    "ring2_id",
+    "ring2_residue",
+    "ring2_centroid",
+    "interaction_type",
+    "interacting_entites",
+]
+
+ARI_FIELDS = [
+    "atom",
+    "ring_id",
+    "ring_centroid",
+    "interaction_type",
+]
+
+RING_FIELDS = [
+    "ring_id",
+    "ring_residue",
+    "ring_centroid",
 ]
